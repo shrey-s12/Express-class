f5e24ec7f861c10916ad1b408aa841188e67d5a8
Shrey Singhal <shreynbd12@gmail.com>
2024-12-29 15:58:57 +0530

    Student Route

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World');
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
